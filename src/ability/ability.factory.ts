933d82ecb29ce39d467a851d107aa7346a59e25e
farhadfahri <91671369+farhadfahri@users.noreply.github.com>
2022-08-07 08:07:47 +0300

    adding subjects and actions

--- src/ability/ability.factory.ts
+++ src/ability/ability.factory.ts
@@ -1,1 +1,27 @@
-export class AbilityFactory {}
+import { InferSubjects, Ability } from '@casl/ability';
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
+
+export enum Action {
+  Manage = 'manage', // wildcard for any action
+  Create = 'create',
+  Read = 'read',
+  Update = 'update',
+  Delete = 'delete',
+}
+
+// connecting actions to specific subjects, can use union of subjects
+// type Subjects = 'Article' | 'Comment' | 'User';
+
+export type Subjects = InferSubjects<typeof User> | 'all';
+
+// join actions and subjects to define abilities
+
+export type AppAbility = Ability<[Action, Subjects]>;
+
+@Injectable()
+export class AbilityFactory {
+  difineAbility(user: User) {
+    // define rules here
+  }
+}
